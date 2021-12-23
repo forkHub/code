3dcda2df1680c6a6ba86c85726d0efd6cc82927b
Rokhman, Fajar <Fajar.Rokhman@verint.com>
2021-12-23 08:42:25 +0700

    hapus insert

--- src/Modul.ts
+++ src/Modul.ts
@@ -40,43 +40,36 @@ export class Modul {
         return -1;
     }
 
-    insert(modulDiInsert: IModul, modulParent: IModul, idxModulDipilih: number) {
-
-        // modulDiInsert;
-        // idDipilih;
-        // cont;
-
-        let idx: number = this.getIdx(idxModulDipilih);
-        this._list.splice(idx, 0, modulDiInsert);
-
-        this.renderSub(modulParent);
-
-        // this._list.splice(idDipilih, 0, modul);
-
-        // let elNext: HTMLElement;
-        // let modulDipilih: IModul = this.getById(idDipilih);
-
-        // if (modulDipilih) {
-        //     elNext = modulDipilih.el.elHtml.nextSibling as HTMLElement;
-        // }
-
-        // cont.insertBefore(modul.el.elHtml, elNext);
-
-        // this._list.splice(idDipilih, 0, modul);
-
-        // console.debug('insert modul, idDipilih: ' + idDipilih + "/idx: " + this.getIdx(modul.id));
-        // console.debug(modul.el.elHtml.parentNode.children[this.getIdx(modul.id)]);
-        // console.debug(modul.el.elHtml);
-    }
+    // insert(modulDiInsert: IModul, modulParent: IModul, idxModulDipilih: number) {
+    //     let idx: number = this.getIdx(idxModulDipilih);
+    //     this._list.splice(idx, 0, modulDiInsert);
+    //     this.renderSub(modulParent);
+    // }
 
     renderModulView(modul: IModul): void {
         modul.el = new ModulView(modul.id);
         modul.el.nama.innerText = modul.nama;
     }
 
+    getSub(modul: IModul): IModul[] {
+        let hasil: IModul[] = [];
+
+        this._list.forEach((item: IModul) => {
+            if (item.induk == modul.id) {
+                if (item.type == Type.TY_MODUL) {
+                    hasil.push(item);
+                }
+            }
+        });
+
+        return hasil;
+    }
+
     renderSub(modul: IModul): void {
-        modul.el.modulCont.innerHTML = '';
-        modul.sub.forEach((sub: IModul) => {
+        let sub: IModul[] = this.getSub(modul);
+
+        sub.forEach((sub: IModul) => {
+            sub.el.detach();
             sub.el.attach(modul.el.modulCont);
         });
     }
@@ -89,35 +82,11 @@ export class Modul {
             let nama: string = '';
             let modulBaru: IModul;
 
-            // modulBaru = {
-            //     id: code.id.id,
-            //     nama: nama,
-            //     type: Type.TY_MODUL,
-            //     induk: modulInduk.id
-            // }
-
             nama = window.prompt('nama modul:');
             modulBaru = this.buat(nama, modulInduk.id);
+            this._list.push(modulBaru);
+            this.renderSub(modulInduk);
 
-            // this.renderModulView(modulBaru);
-
-            // this.insert(modulBaru, modulInduk.id, code.halDaftarStatement.cont);
-
-            // modulBaru.el.elHtml.onclick = () => {
-            //     if (code.state.aktif == State.ST_AWAL || (code.state.aktif == State.ST_MODUL_DIPILIH)) {
-            //         document.body.querySelectorAll('div.modul').forEach((el: Element) => {
-            //             el.classList.remove('dipilih');
-            //         });
-
-            //         code.elAktif = modulBaru.id;
-            //         code.state.aktif = State.ST_MODUL_DIPILIH;
-            //         modulBaru.el.elHtml.classList.add('dipilih');
-            //         code.menu.render2(this.tombolDipilih(modulBaru.id));
-            //     }
-            //     else {
-            //         console.debug('modul di klik batal, state: ' + code.state.aktif);
-            //     }
-            // }
         }));
 
         hasil.push(Tombol.buat(' hapus ', () => {
